ded30cce075c70ae3b3cdcd1d5a9a52d754b3f59
filipewelton1 <filipewelton1@gmail.com>
2024-05-11 20:21:28 -0300

    refactor: add deletion of existing contacts

--- src/use-cases/users/delete-user.spec.ts
+++ src/use-cases/users/delete-user.spec.ts
@@ -2,27 +2,32 @@ import { faker } from '@faker-js/faker'
 import { execSync } from 'child_process'
 import { beforeEach, describe, expect, it } from 'vitest'
 
+import { ContactsRepository } from '__repositories/in-memory/contacts-repository'
 import { UsersRepository } from '__repositories/in-memory/users-repository'
 import { createUser } from '__tests/factories/user-creation'
 import { ResourceNotFoundError } from '__utils/errors/resource-not-found'
 
 import { DeleteUser } from './delete-user'
 
-let repository: UsersRepository
+let usersRepository: UsersRepository
+let contactsRepository: ContactsRepository
 let deleteUser: DeleteUser
 
 beforeEach(() => {
-  repository = new UsersRepository()
-  deleteUser = new DeleteUser(repository)
+  usersRepository = new UsersRepository()
+  contactsRepository = new ContactsRepository()
+  deleteUser = new DeleteUser(usersRepository, contactsRepository)
 })
 
 describe('User deletion', () => {
   it('should be able to delete', async () => {
     execSync('npm run knex migrate:latest')
 
-    const { id } = await createUser({ repository })
+    const { id } = await createUser({ repository: usersRepository })
 
     await deleteUser.execute(id)
+
+    expect(usersRepository.findById(id)).rejects.toBeInstanceOf(Error)
 
     execSync('npm run knex migrate:rollback --all')
   })
--- src/use-cases/users/delete-user.ts
+++ src/use-cases/users/delete-user.ts
@@ -1,16 +1,28 @@
+import { ContactsRepository } from '__repositories/contacts-repository'
 import { UsersRepository } from '__repositories/users-repository'
 import { ResourceNotFoundError } from '__utils/errors/resource-not-found'
 
 import { UseCase } from '../use-case'
 
 export class DeleteUser implements UseCase {
-  constructor(private repository: UsersRepository) {}
+  constructor(
+    private usersRepository: UsersRepository,
+    private contactsRepository: ContactsRepository,
+  ) {}
 
   async execute(id: string) {
-    const user = await this.repository.findById(id)
+    const user = await this.usersRepository.findById(id)
 
     if (!user) throw new ResourceNotFoundError('User not found.')
 
-    await this.repository.delete(id)
+    const contacts = await this.contactsRepository.findManyByUserId(id)
+
+    if (contacts.length !== 0) {
+      for await (const { id } of contacts) {
+        await this.contactsRepository.delete(id)
+      }
+    }
+
+    await this.usersRepository.delete(id)
   }
 }
